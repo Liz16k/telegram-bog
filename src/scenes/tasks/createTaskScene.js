34d465e5b869c37c7fe7f82ae07e643cc100cce6
Liz16k <prohoren4ik@gmail.com>
2023-07-28 01:58:59 +0300

    feat: rewrite to scenes and add extra steps for task creating

--- src/scenes/tasks/createTaskScene.js
+++ src/scenes/tasks/createTaskScene.js
@@ -1,31 +1,77 @@
-const { Scenes } = require("telegraf");
-const { Tasks } = require("../../models/Tasks");
+const { Scenes, Markup } = require("telegraf");
+const { saveTaskToDB } = require("../../services/taskService");
 
-const createTaskScene = new Scenes.BaseScene("createTask");
+const createTaskScene = new Scenes.WizardScene(
+  "createTask",
+  (ctx) => {
+    ctx.reply("Введите задачу:");
+    return ctx.wizard.next();
+  },
+  (ctx) => {
+    ctx.wizard.state.status = "todo";
+    ctx.wizard.state.name = ctx.message.text;
+    ctx.reply(
+      "Хотите установить напоминание?",
+      Markup.inlineKeyboard([
+        Markup.button.callback("Да", "REMINDER_YES"),
+        Markup.button.callback("Нет", "REMINDER_NO"),
+      ])
+    );
+    return ctx.wizard.next();
+  },
+  async (ctx) => {
+    const userId = ctx.update.callback_query.from.id;
+    if (ctx.callbackQuery.data === "REMINDER_YES") {
+      ctx.wizard.state.reminder = true;
+      ctx.reply(
+        'Введите крайний срок выполнения задачи (например, "2023-12-31 23:59"):'
+      );
+      return ctx.wizard.next();
+    } else {
+      ctx.wizard.state.reminder = false;
+      await saveTaskToDB(userId, ctx.wizard.state);
+      ctx.reply("Задача успешно создана!");
+      return ctx.scene.leave();
+    }
+  },
+  async (ctx) => {
+    ctx.wizard.state.dueDate = ctx.message.text;
+    ctx.reply(
+      "Как часто вы хотите получать уведомление о задаче? (каждые ... часов):",
+      Markup.inlineKeyboard([
+        Markup.button.callback("Ежедневно", "DAILY"),
+        Markup.button.callback("Каждые полчаса", "HALF_HOURLY"),
+        Markup.button.callback("Каждый час", "HOURLY"),
+      ])
+    );
+    return ctx.wizard.next();
+  },
+  async (ctx) => {
+    const userId = ctx.update.callback_query.from.id;
+    let interval;
+    switch (ctx.callbackQuery.data) {
+      case "DAILY": {
+        ctx.wizard.state.interval = 24;
+        break;
+      }
+      case "HOURLY": {
+        ctx.wizard.state.interval = 1;
+        break;
+      }
+      case "HALF_HOURLY": {
+        ctx.wizard.state.interval = 0.5;
+        break;
+      }
+      default: {
+        interval = ctx.message.text;
+        if (!isNaN(interval)) ctx.wizard.state.interval = interval;
+      }
+    }
+    await saveTaskToDB(userId, ctx.wizard.state);
+    ctx.reply("Задача успешно создана!");
 
-createTaskScene.enter((ctx) => {
-  ctx.reply("Введите задачу:");
-});
-
-createTaskScene.on("message", async (ctx) => {
-  const userId = ctx.message.from.id;
-  const text = ctx.message.text;
-
-  const userDoc = await Tasks.findOne({ userId });
-  console.log("userDOC", !!userDoc);
-  if (userDoc) {
-    userDoc.tasks = [...userDoc.tasks, { text, status: "todo" }];
-    userDoc.save();
-  } else {
-    const newUserDoc = new Tasks({
-      userId,
-      tasks: [{ text, status: "todo" }],
-    });
-    newUserDoc.save();
+    return ctx.scene.leave();
   }
-
-  ctx.reply("Задача создана успешно!");
-  ctx.scene.leave();
-});
+);
 
 module.exports = { createTaskScene };
